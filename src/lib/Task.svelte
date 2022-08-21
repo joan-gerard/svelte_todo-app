ab56533af1f503bfdc6e5e897df2ad67bad3e026
Joan Gerard <joan.gerard@outlook.com>
2022-08-21 19:15:52 +0100

    display and filter tasks by completion

---
+++ src/lib/Task.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let todo;
+
+  let listItemStyle =
+    "flex max-h-16 my-2 items-center bg-white py-9 px-3 border rounded-lg shadow-md hover:shadow-lg cursor-pointer ";
+  let imgContainerStyle = "bg-orange-400 rounded-full p-3 w-14 mr-4";
+</script>
+
+<li class={listItemStyle}>
+  <div id="image-container" class={imgContainerStyle}>
+    <img src={todo.image} alt={todo.category} />
+  </div>
+  <p>{todo.task}</p>
+</li>
+
+<style>
+  img {
+    height: 100%;
+    margin: 0;
+  }
+</style>
---
+++ src/lib/Tasks.svelte
@@ -0,0 +1,70 @@
+<script>
+  import Task from "./Task.svelte";
+
+  let todos = [
+    {
+      id: 1,
+      category: "groceries",
+      image:
+        "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-groceries-foodies-flaticons-lineal-color-flat-icons-2.png",
+      task: "Buy Bread",
+      completed: true,
+    },
+    {
+      id: 2,
+      category: "studying",
+      image:
+        "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-studying-university-flaticons-flat-flat-icons-4.png",
+      task: "Study for exam",
+      completed: false,
+    },
+    {
+      id: 3,
+      category: "friends",
+      image:
+        "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-friends-relationship-flaticons-lineal-color-flat-icons-3.png",
+      task: "Meet John for coffee",
+      completed: false,
+    },
+  ];
+
+  let filteredTasks = todos;
+
+  const filterTasks = (e) => {
+    if (e.target.value === "null") {
+      filteredTasks = todos;
+      return;
+    }
+    const completed = e.target.value === "true";
+    filteredTasks = todos.filter((todo) => {
+      return todo.completed === completed;
+    });
+  };
+</script>
+
+<div id="app-container" class="p-8">
+  <h1 class="text-2xl text-center ">My Tasks</h1>
+  <label for="task-filter">Filter tasks</label>
+  <select
+    on:change={filterTasks}
+    name="task-filter"
+    id="task-filter"
+    class="my-5 mx-2 border rounded-md p-1"
+  >
+    <option value={null}>All</option>
+    <option value={true}>Completed</option>
+    <option value={false}>Pending</option>
+  </select>
+  <div id="task-list__container" class="">
+    {#each filteredTasks as todo, i}
+      <ul id="task-list">
+        <Task {todo} />
+      </ul>
+    {:else}
+      <p>Your list is empty</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+</style>
